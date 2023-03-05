d2e24f169fe550ef5347bfb76f6d850a0c0cc4b6
Glazapolzet <VypZlol@yandex.ru>
2023-03-05 04:37:13 +0300

    lol

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const ghoulArr = [];
+let n = '1000';
+let i = 0;
+let timerId;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+while (parseInt(n) > 0) {
+  ghoulArr.push(`${n} - 7 = ${parseInt(n) - 7}`);
+  n = n-7;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function callGhoul () {
+  if (i >= ghoulArr.length) {
+    root.render(
+      <div>
+        <p className="kaneki">I'm ghoul</p>
+        <img src="https://media.tenor.com/wvDyYLTH4vEAAAAC/zxc-cat.gif" alt="zxc"/>
+      </div>
+    );
+    clearInterval(timerId);
+    return;
+  }
+  root.render(
+    <div>
+      <button>LET ME DIE</button>
+      <p>{ghoulArr[i]}</p>
+    </div>
+  );
+  i++;
+}
+
+root.render(
+  <div>
+    <button onClick={() => timerId = setInterval(callGhoul, 37)}>LET ME DIE</button>
+  </div>
+)
+
 reportWebVitals();
